907fa7a0d5bca52239be03353171afced3ff667f
dang1509 <minhdang15092002@gmail.com>
2024-11-10 15:30:12 +0700

    Sửa file

--- commons/env.php
+++ commons/env.php
@@ -1,9 +1,9 @@
 <?php
- define('BASE_URL','http://localhost/du_an_1/');
- define('BASE_URL_ADMIN','http://localhost/du_an_1/admin/');
+ define('BASE_URL','http://localhost/du_an_trang_suc/');
+ define('BASE_URL_ADMIN','http://localhost/du_an_trang_suc/admin/');
  define('DB_HOST','localhost');
  define('DB_PORT',3306);
  define('DB_USERNAME','root');
  define('DB_PASSWORD','');
- define('DB_NAME','db_duanmau');
+ define('DB_NAME','db_duan1');
  define('PATH_ROOT'    , __DIR__ . '/../'); 
